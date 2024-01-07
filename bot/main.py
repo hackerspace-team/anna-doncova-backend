c7069455d468802b8076b9f92bcb35e8d894c592
romka-best <romka.best2016@yandex.ru>
2024-01-06 20:47:27 -0800

    Release 0.2.1

--- bot/main.py
+++ bot/main.py
@@ -1,3 +1,5 @@
+import logging
+
 import nest_asyncio
 import asyncio
 from dotenv import load_dotenv
@@ -8,31 +10,49 @@ from telegram.ext import Application, CommandHandler
 from AnnaDoncovaBot.handlers.command_handlers import applications
 from AnnaDoncovaBot.settings import TELEGRAM_TOKEN
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
 nest_asyncio.apply()
 load_dotenv()
 
 
 async def start_telegram_bot():
-    application = Application.builder().token(TELEGRAM_TOKEN).build()
+    try:
+        application = Application.builder().token(TELEGRAM_TOKEN).build()
 
-    application.add_handler(CommandHandler("applications", applications))
+        application.add_handler(CommandHandler("applications", applications))
 
-    await application.run_polling()
+        await application.bot.delete_webhook()
+
+        await application.run_polling()
+    except Exception as e:
+        logger.error(f"Ошибка при запуске Telegram бота: {e}")
 
 
 async def web_app():
-    app = web.Application()
-    app.router.add_get('/', lambda request: web.Response(text="Hello, I'm running!"))
-    return app
+    try:
+        app = web.Application()
+        app.router.add_get('/', lambda request: web.Response(text="Hello, I'm running!"))
+
+        return app
+    except Exception as e:
+        logger.error(f"Ошибка при создании веб-приложения: {e}")
 
 
 async def main():
-    web_server = web.AppRunner(await web_app())
-    await web_server.setup()
-    site = web.TCPSite(web_server, '0.0.0.0', int(os.environ.get("PORT", 8080)))
-    await site.start()
+    try:
+        web_server = web.AppRunner(await web_app())
+        await web_server.setup()
 
-    await start_telegram_bot()
+        site = web.TCPSite(web_server, '0.0.0.0', int(os.environ.get("PORT", 8080)))
+        await site.start()
+
+        await start_telegram_bot()
+    except Exception as e:
+        logger.error(f"Ошибка при запуске основной функции: {e}")
 
 
 if __name__ == "__main__":
